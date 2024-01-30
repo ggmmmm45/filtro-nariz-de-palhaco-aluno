3eecef4a2cd4f9c8d6f013d609d25c68948ce30c
miguel <miguelmastrangelof@email.com>
2024-01-30 17:54:16 -0300

    update
    update

--- main.js
+++ main.js
@@ -2,20 +2,24 @@ noseX=0;
 noseY=0;
 
 // Primeiro definimos uma função preload() 
-
- 
+function preload()
+ {
+  clown_nose = loadImage('https://i.postimg.cc/sgdjRD8n/Design-sem-nome.png');
+ }
 
 
 // Vamos criar uma função setup() e dentro dela criamos uma canvas (tela)
 function setup() {
-  
+  canvas = createCanvas(300, 300);
   // colocaremos canvas no centro da página usando
-
+  canvas.center();
   video = createCapture(VIDEO);
   video.size(300, 300);
   video.hide();
 
  
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
   
 }
 
@@ -28,8 +32,8 @@ function gotPoses(results)
   
   {
     console.log(results);
-    noseX = results[0].pose.nose.x-15;
-    noseY = results[0].pose.nose.y-15;
+    faceX = results[0].pose.face.x-15;
+    faceY = results[0].pose.face.y-15;
   }
 }
 // Vamos definir uma função draw() e a deixá-la vazia, nas próximas aulas iremos adicionar o código
@@ -37,12 +41,12 @@ function gotPoses(results)
 // draw()
 
 function draw() {
-  
-  image(clown_nose, noseX, noseY, 30, 30);
+ image(video, 0, 0, 300, 300);
+  image(clown_nose, faceX, faceY, 200, 200);
 }
 // Agora vamos definir a função takeSnapshot(), que chamamos ao clicar no botão Tirar Foto
 function take_snapshot(){   
   
 //  Dentro da função save() temos que escrever o nome no qual queremos salvar o arquivo. 
- 
+ save('myFilterImage.png');
 }
