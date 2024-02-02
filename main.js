c225d35c13638f67571488175f65998b4723be0a
miguel <miguelmastrangelof@email.com>
2024-02-02 17:47:22 -0300

    update
    update

--- main.js
+++ main.js
@@ -1,52 +1,59 @@
-noseX=0;
-noseY=0;
-
-// Primeiro definimos uma função preload() 
-function preload()
- {
-  clown_nose = loadImage('https://i.postimg.cc/sgdjRD8n/Design-sem-nome.png');
- }
+let clownFace;
+let faceX = 0;
+let faceY = 0;
 
 
-// Vamos criar uma função setup() e dentro dela criamos uma canvas (tela)
+function preload() {
+  clownFace = loadImage('https://i.postimg.cc/hPh18nDh/Design-sem-nome.png');
+}
+
+
 function setup() {
   canvas = createCanvas(300, 300);
-  // colocaremos canvas no centro da página usando
   canvas.center();
   video = createCapture(VIDEO);
   video.size(300, 300);
   video.hide();
 
- 
+
   poseNet = ml5.poseNet(video, modelLoaded);
   poseNet.on('pose', gotPoses);
-  
 }
+
 
 function modelLoaded() {
   console.log('PoseNet Is Initialized');
 }
 
-function gotPoses(results)
-{
-  
-  {
+
+function gotPoses(results) {
+  if (results.length > 0) {
     console.log(results);
-    faceX = results[0].pose.face.x-15;
-    faceY = results[0].pose.face.y-15;
+    faceX = results[0].pose.nose.x - 15;
+    faceY = results[0].pose.nose.y - 15;
   }
 }
-// Vamos definir uma função draw() e a deixá-la vazia, nas próximas aulas iremos adicionar o código
-// de acesso da webcam, para desenhar a imagem do nariz de palhaço na tela dentro da função
-// draw()
+
 
 function draw() {
- image(video, 0, 0, 300, 300);
-  image(clown_nose, faceX, faceY, 200, 200);
+  image(video, 0, 0, 300, 300);
+
+
+  // Ajuste as coordenadas para centralizar a imagem no rosto
+  const imageSize = 250;
+  const faceCenterX = faceX - imageSize / 2;
+  const faceCenterY = faceY - imageSize / 2;
+
+
+  image(clownFace, faceCenterX, faceCenterY, imageSize, imageSize);
+
+
+
 }
-// Agora vamos definir a função takeSnapshot(), que chamamos ao clicar no botão Tirar Foto
-function take_snapshot(){   
-  
-//  Dentro da função save() temos que escrever o nome no qual queremos salvar o arquivo. 
- save('myFilterImage.png');
+
+
+function take_snapshot() {
+  save('myFilterImage.png');
 }
+
+
